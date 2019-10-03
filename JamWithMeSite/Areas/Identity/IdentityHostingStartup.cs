8b6bacb3b619682b0e5f4938217293cf8cec404c
Kevin Stone <ruca6616@gmail.com>
2019-10-03 16:14:28 -0700

    Ran the Identity Scaffolder

---
+++ JamWithMeSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JamWithMeSite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JamWithMeSite.Areas.Identity.IdentityHostingStartup))]
+namespace JamWithMeSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
