a6549aaf5170485ad91584b6e80bec91bbe4451e
Kevin Stone <ruca6616@gmail.com>
2019-10-03 16:17:08 -0700

    Added custom data to the IdentityDB and updated the Input Model

---
+++ JamWithMeSite/Areas/Identity/Data/JamWithMeSiteUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace JamWithMeSite.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the JamWithMeSiteUser class
+    public class JamWithMeSiteUser : IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+        [PersonalData]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
