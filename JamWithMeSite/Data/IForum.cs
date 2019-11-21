1ddcf1dc4c506b9838271c14a86a8e57232c321a
Kevin Stone <ruca6616@gmail.com>
2019-11-20 18:10:28 -0800

    Created forum interface to implement.

---
+++ JamWithMeSite/Data/IForum.cs
@@ -0,0 +1,22 @@
+﻿using JamWithMeSite.Areas.Identity.Data;
+using JamWithMeSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamWithMeSite.Data
+{
+    public interface IForum
+    {
+
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<JamWithMeSiteUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
